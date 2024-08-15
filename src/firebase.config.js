cd07621a0f52e2eb20a9a88a7feb4dde2b7b1534
ravi-756 <ravidhundhara2000.gmail>
2024-08-15 20:25:45 +0530

    working

---
+++ src/firebase.config.js
@@ -0,0 +1,56 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import {getFirestore} from "firebase/firestore"
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDDR40ZGdrNG-gNjrcxJESKriKDmiYRx8c",
+//   authDomain: "house-market-48cae.firebaseapp.com",
+//   projectId: "house-market-48cae",
+//   storageBucket: "house-market-48cae.appspot.com",
+//   messagingSenderId: "930884576925",
+//   appId: "1:930884576925:web:1ff8a3f4558b811c339674"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore();
+
+
+// 
+// new id
+
+
+// import { initializeApp } from "firebase/app";
+// import {getFirestore} from 'firebase/firestore'
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBKlof6AxagpqNu1Ncl60DLvSxGnDGJ7Nk",
+//   authDomain: "house-market-509fc.firebaseapp.com",
+//   projectId: "house-market-509fc",
+//   storageBucket: "house-market-509fc.appspot.com",
+//   messagingSenderId: "957848139377",
+//   appId: "1:957848139377:web:1fe2a1c245e7770f9c4dd0"
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore();
+
+// after
+
+// // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfLmKzw9Ll3AEQD_VTpsGIpsvhrSNkLII",
+  authDomain: "house-market-42add.firebaseapp.com",
+  projectId: "house-market-42add",
+  storageBucket: "house-market-42add.appspot.com",
+  messagingSenderId: "152817175493",
+  appId: "1:152817175493:web:0a1c89ded3535ce52d19ac"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
